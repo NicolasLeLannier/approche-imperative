6f7f969101013903e4538302ef70723926974eee
NicolasLeLannier <lelannier.nicolas@gmail.com>
2023-10-05 11:07:41 +0200

    Exo Comparaison tableau

---
+++ src/fr/algorithmie/ComparaisonTableau.java
@@ -0,0 +1,26 @@
+package fr.algorithmie;
+
+public class ComparaisonTableau {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// Initialisation des tableaux
+		int[] array1 = { 1, 15, -3, 0, 8, 7, 4, -2, 28, 7, -1, 17, 2, 3, 0, 14, -4 };
+		int[] array2 = { 3, -8, 17, 5, -1, 4, 0, 6, 2, 11, -5, -4, 8 };
+		
+		int nombreElementCommun = 0;
+		
+		// Double boucle for 
+		for (int i = 0; i < array1.length; i++) {
+			for (int f = 0; f < array2.length; f++) {
+				if(array1[i] == array2[f]) {
+					nombreElementCommun ++;
+				}
+			}
+		}
+		
+		System.out.println("Nombre éléments en communs : " + nombreElementCommun);
+	}
+
+}
