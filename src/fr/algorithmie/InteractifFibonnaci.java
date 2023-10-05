f7e4ff0e0a09f50f9f31fc4311171846e47d3461
NicolasLeLannier <lelannier.nicolas@gmail.com>
2023-10-05 16:17:05 +0200

    Exo InteractifFibonnaci

--- src/fr/algorithmie/InteractifFibonnaci.java
+++ src/fr/algorithmie/InteractifFibonnaci.java
@@ -7,8 +7,23 @@ public class InteractifFibonnaci {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		System.out.println("Suite de Fibonacci !");
+		
 		// Initialisation du scanner
 		Scanner scanner = new Scanner(System.in);
+
+		System.out.print("Choisir un rang : ");
+		int nb = scanner.nextInt();
+		
+		for(int i = 1; i <= nb; i++) {
+			System.out.println("Rang " + i + " => " + TestFibonacci(i));
+		}
 	}
 
+	public static int TestFibonacci(int rang) {
+		if(rang <= 1) 
+			return rang;
+		else 
+			return TestFibonacci(rang-1) + TestFibonacci(rang-2);
+	}
 }
--- src/fr/algorithmie/InteractifStockageNombre.java
+++ src/fr/algorithmie/InteractifStockageNombre.java
@@ -58,22 +58,26 @@ public class InteractifStockageNombre {
 //	public static void ajout() {
 //		Scanner scanner = new Scanner(System.in);
 //		int nb2 = 0;
-//		int[] array = {};
+//		int[] array = new int[0];
 //
 //		System.out.print("\nEntrez un nombre à ajouter : ");
-//		nb2 = scanner.nextInt();
+//		array = Arrays.copyOf(array, array.length + 1);
 //		array[array.length - 1] = nb2;
 //	}
 //
 //	public static void afficher() {
-//		int[] array = { 1, 34, 23 };
+//		int[] array = new int[0];
 //
 //		if (array.length == 0) {
-//			System.out.println("Vous n'avez pas encore de données dans votre tableau.");
+//			System.out.println(
+//					"Vous n'avez pas encore de données dans votre tableau. Vous pouvez en ajouter en appuyant sur 1.");
 //		} else {
+//			// Affiche un tableau avec une bonne présentation visuelle
+//			System.out.println("\n----------- Voici votre tableau -------------");
 //			for (int affichage : array) {
 //				System.out.print(affichage + " ");
 //			}
+//			System.out.println("\n---------------------------------------------");
 //		}
 //	}
 }
