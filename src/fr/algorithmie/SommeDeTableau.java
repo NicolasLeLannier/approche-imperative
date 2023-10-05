fa88a683cec5b2c45e1ea5ac1e07a9bb5305e507
NicolasLeLannier <lelannier.nicolas@gmail.com>
2023-10-05 10:58:44 +0200

    Exo Somme tableau

---
+++ src/fr/algorithmie/SommeDeTableau.java
@@ -0,0 +1,28 @@
+package fr.algorithmie;
+
+public class SommeDeTableau {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// Initialisation du tableau
+		int[] array1 = { 1, 15, -3, 0, 8, 7, 4, -2, 28, 7, -1, 17, 2, 3, 0, 14, -4 };
+		int[] array2 = { -1, 12, 17, 14, 5, -9, 0, 18, -6, 0, 4, -13, 5, 7, -2, 8, -1 };
+		
+		int[] array3 = new int[array1.length + array2.length];
+		int longueur = array1.length + array2.length;
+		
+		for (int i = 0; i < longueur; i++) {
+			if(i >= array1.length) {
+				array3[i] = array2[i - array1.length];
+			} else {
+				array3[i] = array1[i];
+			}
+		}
+		
+		
+		for (int affichage : array3)
+			System.out.print(affichage + " ");
+	}
+
+}
