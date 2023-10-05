4f7a3a0c234ec0c609e5bf955aa9ea78fbab017d
NicolasLeLannier <lelannier.nicolas@gmail.com>
2023-10-05 15:56:17 +0200

    Exo InteractifStockageNombre

---
+++ src/fr/algorithmie/InteractifStockageNombre.java
@@ -0,0 +1,77 @@
+package fr.algorithmie;
+
+import java.util.*;
+
+public class InteractifStockageNombre {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// Initialisation du scanner
+		Scanner scanner = new Scanner(System.in);
+		int nb1 = 0;
+		int nb2 = 0;
+		int[] array = {};
+
+		// Tant que le nombre n'est pas entre 1 et 10 on repose la question
+		do {
+			System.out.println("\n1. Ajouter un nombre \n2. Afficher les nombres existants \n3. Fermer le programme");
+			System.out.print("Entrez un nombre : ");
+			nb1 = scanner.nextInt();
+
+			switch (nb1) {
+			case 1:
+//				ajout();
+
+				System.out.print("\nEntrez un nombre à ajouter : ");
+				nb2 = scanner.nextInt();
+
+				array = Arrays.copyOf(array, array.length + 1);
+				array[array.length - 1] = nb2;
+
+				break;
+			case 2:
+//				afficher();
+
+				if (array.length == 0) {
+					System.out.println(
+							"Vous n'avez pas encore de données dans votre tableau. Vous pouvez en ajouter en appuyant sur 1.");
+				} else {
+					System.out.println("\n----------- Voici votre tableau -------------");
+					for (int affichage : array) {
+						System.out.print(affichage + " ");
+					}
+					System.out.println("\n---------------------------------------------");
+				}
+
+				break;
+			default:
+				System.out.println("Au revoir !");
+			}
+
+		} while (nb1 != 3);
+
+	}
+
+//	public static void ajout() {
+//		Scanner scanner = new Scanner(System.in);
+//		int nb2 = 0;
+//		int[] array = {};
+//
+//		System.out.print("\nEntrez un nombre à ajouter : ");
+//		nb2 = scanner.nextInt();
+//		array[array.length - 1] = nb2;
+//	}
+//
+//	public static void afficher() {
+//		int[] array = { 1, 34, 23 };
+//
+//		if (array.length == 0) {
+//			System.out.println("Vous n'avez pas encore de données dans votre tableau.");
+//		} else {
+//			for (int affichage : array) {
+//				System.out.print(affichage + " ");
+//			}
+//		}
+//	}
+}
